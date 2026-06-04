9da416206dcfa1bcb6dffce6f026cb8ab19307fc
Imad <imadeddine.boucenna@gmail.com>
2026-06-04 13:00:41 +0100

    add build.sh for render

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate#!/usr/bin/env bash
+set -o errexit
+
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
